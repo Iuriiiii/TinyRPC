bdc53b451f4e914bdb6dc2cbef4de216aab4cbab
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-10-20 21:57:07 -0300

    fix: attempt to fix compiler

--- src/tools/compiler/build-utils.compile.ts
+++ src/tools/compiler/build-utils.compile.ts
@@ -1,11 +1,14 @@
-import { resolve } from "jsr:path";
-
-export function buildUtils(host?: string) {
-  const currentPath = import.meta.dirname!;
+export function buildUtils(
+  host?: string,
+  currentPath = import.meta.dirname,
+) {
+  if (!currentPath) {
+    throw new Error("No current path to build tools");
+  }
   const output = new TextDecoder()
     .decode(
       Deno.readFileSync(
-        `${resolve(currentPath)}/assets/rpc.util.ts`,
+        "./src/tools/compiler/assets/rpc.util.ts",
       ),
     );
 

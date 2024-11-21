026ae6dad34f7d390dc829071a20c03f43dd6715
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-11-21 20:20:53 -0300

    server info to compilers

--- src/interfaces/compiler-options.interface.ts
+++ src/interfaces/compiler-options.interface.ts
@@ -1,7 +1,9 @@
 import type { SdkOptions } from "./sdk-options.interface.ts";
 import type { ServerMetadata } from "./server-metadata.interface.ts";
+import type { ServerOptions } from "./server-options.interface.ts";
 
 export interface CompilerOptions {
   metadata: ServerMetadata;
   sdkOptions?: Partial<SdkOptions>;
+  server?: Partial<ServerOptions>;
 }

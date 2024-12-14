af0eb7b31b53563f20973020bca972b106477f9c
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-12-14 04:55:08 -0300

    fix getEnum

--- src/utils/get-enum.util.ts
+++ src/utils/get-enum.util.ts
@@ -1,5 +1,5 @@
 import { enums } from "../singletons/mod.ts";
 
 export function getEnum<T extends object | undefined>(name: string): T {
-  return enums.find((_enum) => _enum.name === name)!.value as T;
+  return enums.find((_enum) => _enum.name === name)?.value as T;
 }

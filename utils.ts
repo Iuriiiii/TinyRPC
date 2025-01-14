72a12160dc1d3d3aa16d71a1188d0b4c3da22fd2
Iuriiiiiii <alex.casas@webtroniclabs.com>
2025-01-13 22:03:49 -0300

    export more util types

--- utils.ts
+++ utils.ts
@@ -2,6 +2,22 @@ export * from "./src/datatypes/utils/mod.ts";
 export * from "./src/utils/crash-if.util.ts";
 export * from "./src/utils/crash-if-not.util.ts";
 
+export * from "./src/utils/log.util.ts";
+export * from "./src/utils/log-if.util.ts";
+export * from "./src/utils/log-if-not.util.ts";
+
+export * from "./src/utils/warn.util.ts";
+export * from "./src/utils/warn-if.util.ts";
+export * from "./src/utils/warn-if-not.util.ts";
+
+export * from "./src/utils/debug.util.ts";
+export * from "./src/utils/debug-if.util.ts";
+export * from "./src/utils/debug-if-not.util.ts";
+
+export * from "./src/utils/info.util.ts";
+export * from "./src/utils/info-if.util.ts";
+export * from "./src/utils/info-if-not.util.ts";
+
 export * from "./src/utils/get-structure.util.ts";
 export * from "./src/utils/get-module.util.ts";
 export * from "./src/utils/get-param.util.ts";

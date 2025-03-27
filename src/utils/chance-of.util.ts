fff6de06d240a39e3d85c7d15689e4099ac7f2e3
Iuriiiiiii <alex.casas@webtroniclabs.com>
2025-03-27 18:27:44 -0300

    chore: fmt

--- src/utils/chance-of.util.ts
+++ src/utils/chance-of.util.ts
@@ -2,17 +2,17 @@ import { crashIfNot } from "./crash-if-not.util.ts";
 
 /**
  * Returns true with a given probability.
- * 
+ *
  * @param unaryChance The probability of returning true (0 to 1)
  * @param inclusive If true (default), uses <= comparison; if false, uses <
  * @returns boolean - true with the specified probability
- * 
+ *
  * @example
  * // 50% chance
  * if (chanceOf(0.5)) {
  *   console.log("50% chance hit");
  * }
- * 
+ *
  * @example
  * // Exactly 1 in 255 chance (exclusive)
  * if (chanceOf(1/255, false)) {
